cf1c5347522c22952724abc6af5a0d4787a11b0b
Marcus Morua <91860506+mmorua@users.noreply.github.com>
2022-01-23 14:49:27 -0600

    feat: Add name, id, email properties ; Add getName(), getEmail(), getRole() methods to Employee class using Jest

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,39 @@
+// Using Employee constructor
 const Employee = require('../lib/Employee.js');
-
-test('creates a employee object', () => {
-    const employee = new Employee('Marcus', 'email');
+test('creates an employee object', () => {
+    const employee = new Employee('Marcus', 8, 'marcus.morua@gmail.com');
     console.log(employee);
-    expect(employee.name).toBe('Marcus');
-    expect(employee.id).toEqual(1);
-    expect(employee.email).toBe('email');
-    expect(employee.role).toBe(expect.arrayContaining(['Employee', 'Manager', 'Engineer', 'Intern']));
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// getName() method
+test('gets employee name', () => {
+    const employee = new Employee('Marcus', 8, 'marcus.morua@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+
+// getId() method
+test('gets employee ID', () => {
+    const employee = new Employee('Marcus', 8, 'marcus.morua@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// getEmail() method
+test('gets employee email', () => {
+    const employee = new Employee('Marcus', 8, 'marcus.morua@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+// getRole() method
+test('gets employee role', () => {
+    const employee = new Employee('Marcus', 8, 'marcus.morua@gmail.com');
+
+    expect(employee.getRole()).toEqual('Employee');
 });
