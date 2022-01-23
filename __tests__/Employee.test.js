aa88a02a25f3a8a683642e44c6eaeb88b59f31f9
Marcus Morua <91860506+mmorua@users.noreply.github.com>
2022-01-22 20:06:58 -0600

    AskBCS assistance

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,10 @@
-const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 test('creates a employee object', () => {
     const employee = new Employee('Marcus', 'email');
-
+    console.log(employee);
     expect(employee.name).toBe('Marcus');
-    expect(employee.id).toEqual(Number);
+    expect(employee.id).toEqual(1);
     expect(employee.email).toBe('email');
     expect(employee.role).toBe(expect.arrayContaining(['Employee', 'Manager', 'Engineer', 'Intern']));
 });
